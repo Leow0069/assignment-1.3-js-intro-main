09239d093a47c685fa97c7dd674d606cf40ea073
Leow0069 <hansen_leow@hotmail.com>
2021-08-06 15:29:33 +0800

    Update1

---
+++ assignment/Celsius to KF.js
@@ -0,0 +1,10 @@
+const celsius = 0; // Add some code to get the user input
+
+// Conversion Code
+const celsiusToFahrenheit = celsius * 9/5 +32;
+const celsiusToKelvin = celsius + 273.15;
+
+// Print the results to the browser console
+console.log(`Celsius: ${celsius} C`);
+console.log(`Fahrenheit: ${celsiusToFahrenheit} F`);
+console.log(`Kelvin: ${celsiusToKelvin} K`);
